39836bc914ed931fd47d6802103925d005c004c7
Pedro Pinho <ptfpinho@hotmail.com>
2023-10-04 15:00:27 +0200

    update vine - add cleanup + small fixes

--- Synthesis/vine.py
+++ Synthesis/vine.py
@@ -7,10 +7,11 @@ class Vine:
     
     def __init__(self):
         self.havens: dict[str, Haven] = {}
+        self.networking = VineNetworking()
 
     def create_haven(self, name, capacity):
         haven = Haven(name, capacity)
-        VineNetworking().create_network(name, 'vine_bridge', 'veth1', 'veth2', '10.0.0.3', '24', 'eth0')
+        self.networking.create_network(name)
         self.havens[name] = haven
 
     def delete_haven(self, name):
@@ -18,24 +19,31 @@ class Vine:
             del self.havens[name]
 
 
-    def attach_container_to_haven(self, haven_key, container_name, container_image):
-        if haven_key not in self.havens:
+    def attach_container_to_haven(self, haven_name, container_name, container_image):
+        if haven_name not in self.havens:
             return
-        print(self.havens[haven_key])
-        self.havens[haven_key].run_container(container_name, container_image)
+        print(self.havens[haven_name])
+        container = self.havens[haven_name].run_container(container_name, container_image)
+        self.networking.connect_container_to_network(container, haven_name)
     
 
     def list_containers(self):
         for haven in self.havens.values():
             haven.list_containers()
 
-        
+    def cleanup(self):
+        for haven in self.havens.values():
+            haven.cleanup()
+        vine_networking = VineNetworking()
+        for haven_name in vine_networking.docker_client.networks.list(names=self.havens.keys()):
+            vine_networking.remove_network(haven_name)
+
 def main():
     capacity = Capacity(cpu_cores=4, memory_mb=8192, storage_gb=100)
     vine = Vine()
     vine.create_haven("haven1", capacity)
-    vine.attach_container_to_haven("haven1", "nginx_test1", "nginx:latest")
-    vine.attach_container_to_haven("haven1", "nginx_test2", "nginx:latest")
+    vine.attach_container_to_haven("haven1", "ubuntu_test_1", "ubuntu2")
+    vine.attach_container_to_haven("haven1", "ubuntu-test_2", "ubuntu2  ")
 
     vine.list_containers()
 
